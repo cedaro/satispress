37db219fad3619184e7673a05b2e5b0ed57c3f6c
Brady Vercher <brady@blazersix.com>
2016-10-20 09:31:58 -0700

    Update the version parser with upstream changes.

--- includes/class-satispress-version-parser.php
+++ includes/class-satispress-version-parser.php
@@ -6,9 +6,10 @@
  * @since 0.1.0
  * @author Jordi Boggiano <j.boggiano@seld.be>
  * @link https://github.com/composer/composer/blob/master/src/Composer/Package/Version/VersionParser.php
+ * @link https://github.com/composer/semver/blob/master/src/VersionParser.php
  */
 class SatisPress_Version_Parser {
-	private static $modifierRegex = '[._-]?(?:(stable|beta|b|RC|alpha|a|patch|pl|p)(?:[.-]?(\d+))?)?([.-]?dev)?';
+	private static $modifierRegex = '[._-]?(?:(stable|beta|b|RC|alpha|a|patch|pl|p)((?:[.-]?\d+)*+)?)?([.-]?dev)?';
 
 	/**
 	 * Normalizes a version string to be able to perform comparisons on it.
@@ -24,22 +25,28 @@ class SatisPress_Version_Parser {
 			$fullVersion = $version;
 		}
 
-		// Ignore aliases and just assume the alias is required instead of the source.
-		if ( preg_match( '{^([^,\s]+) +as +([^,\s]+)$}', $version, $match ) ) {
+		// Strip of aliasing.
+		if ( preg_match( '{^([^,\s]++) ++as ++([^,\s]++)$}', $version, $match ) ) {
 			$version = $match[1];
 		}
 
 		// Match master-like branches.
-		if ( preg_match('{^(?:dev-)?(?:master|trunk|default)$}i', $version ) ) {
+		if ( preg_match( '{^(?:dev-)?(?:master|trunk|default)$}i', $version ) ) {
 			return '9999999-dev';
 		}
 
+		// If requirement is branch-like, use full name.
 		if ( 'dev-' === strtolower( substr( $version, 0, 4 ) ) ) {
 			return 'dev-' . substr( $version, 4 );
 		}
 
+		// Strip off build metadata.
+		if ( preg_match( '{^([^,\s+]++)\+[^\s]++$}', $version, $match ) ) {
+			$version = $match[1];
+		}
+
 		// Match classical versioning.
-		if ( preg_match( '{^v?(\d{1,3})(\.\d+)?(\.\d+)?(\.\d+)?' . self::$modifierRegex . '$}i', $version, $matches ) ) {
+		if ( preg_match( '{^v?(\d{1,5})(\.\d++)?(\.\d++)?(\.\d++)?' . self::$modifierRegex . '$}i', $version, $matches ) ) {
 			$version = $matches[1]
 				. ( ! empty( $matches[2] ) ? $matches[2] : '.0' )
 				. ( ! empty( $matches[3] ) ? $matches[3] : '.0' )
@@ -49,7 +56,7 @@ class SatisPress_Version_Parser {
 
 		// Match date-based versioning.
 		elseif ( preg_match( '{^v?(\d{4}(?:[.:-]?\d{2}){1,6}(?:[.:-]?\d{1,3})?)' . self::$modifierRegex . '$}i', $version, $matches ) ) {
-			$version = preg_replace('{\D}', '-', $matches[1]);
+			$version = preg_replace( '{\D}', '.', $matches[1] );
 			$index = 2;
 		}
 
@@ -59,7 +66,7 @@ class SatisPress_Version_Parser {
 				if ( 'stable' === $matches[ $index ] ) {
 					return $version;
 				}
-				$version .= '-' . self::expandStability( $matches[ $index ] ) . ( ! empty( $matches[ $index + 1 ] ) ? $matches[ $index + 1 ] : '' );
+				$version .= '-' . self::expandStability( $matches[ $index ] ) . ( ! empty( $matches[ $index + 1 ] ) ? ltrim( $matches[ $index + 1 ], '.-' ) : '' );
 			}
 
 			if ( ! empty( $matches[ $index + 2 ] ) ) {
@@ -69,12 +76,20 @@ class SatisPress_Version_Parser {
 			return $version;
 		}
 
-		// Match dev branches
+		// Match dev branches.
 		if ( preg_match( '{(.*?)[.-]?dev$}i', $version, $match ) ) {
 			try {
 				return self::normalizeBranch( $match[1] );
 			} catch ( Exception $e ) {}
 		}
+
+		$extraMessage = '';
+		if ( preg_match( '{ +as +' . preg_quote( $version ) . '$}', $fullVersion ) ) {
+			$extraMessage = ' in "' . $fullVersion . '", the alias must be an exact version';
+		} elseif ( preg_match( '{^' . preg_quote( $version ) . ' +as +}', $fullVersion ) ) {
+			$extraMessage = ' in "' . $fullVersion . '", the alias source must be an exact version, if it is a branch name you should prefix it with dev-';
+		}
+		throw new \UnexpectedValueException( 'Invalid version string "' . $version . '"' . $extraMessage );
 	}
 
 	/**
@@ -90,10 +105,10 @@ class SatisPress_Version_Parser {
 			return self::normalize( $name );
 		}
 
-		if ( preg_match( '#^v?(\d+)(\.(?:\d+|[x*]))?(\.(?:\d+|[x*]))?(\.(?:\d+|[x*]))?$#i', $name, $matches ) ) {
+		if ( preg_match( '{^v?(\d++)(\.(?:\d++|[xX*]))?(\.(?:\d++|[xX*]))?(\.(?:\d++|[xX*]))?$}i', $name, $matches ) ) {
 			$version = '';
 			for ( $i = 1; $i < 5; $i++ ) {
-				$version .= isset( $matches[ $i ]) ? str_replace( '*', 'x', $matches[ $i ] ) : '.x';
+				$version .= isset( $matches[ $i ]) ? str_replace( array( '*', 'X' ), 'x', $matches[ $i ] ) : '.x';
 			}
 
 			return str_replace( 'x', '9999999', $version ) . '-dev';
