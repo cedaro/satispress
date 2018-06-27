b7ba76de1bda57cdba37e1e8122b02adb5d85cdc
Brady Vercher <brady@blazersix.com>
2018-06-27 09:29:47 -0700

    Set up local storage during activation.

--- src/Provider/Activation.php
+++ src/Provider/Activation.php
@@ -12,6 +12,7 @@ declare ( strict_types = 1 );
 namespace SatisPress\Provider;
 
 use Cedaro\WP\Plugin\AbstractHookProvider;
+use SatisPress\Storage\Local;
 
 /**
  * Class to activate the plugin.
@@ -37,6 +38,33 @@ class Activation extends AbstractHookProvider {
 	 * @since 0.3.0
 	 */
 	public function activate() {
+		$this->setup_storage();
 		update_option( 'satispress_flush_rewrite_rules', 'yes' );
+	}
+
+	/**
+	 * Set up the local storage provider.
+	 *
+	 * Creates the cache path and adds an .htaccess file to prevent HTTP access
+	 * on Apache.
+	 *
+	 * @since 0.3.0
+	 */
+	public function setup_storage() {
+		$container  = $this->plugin->get_container();
+		$cache_path = $container->get( 'cache.path' );
+		$storage    = $container->get( 'storage' );
+		$htaccess   = $container->get( 'htaccess.handler' );
+
+		if ( ! $storage instanceof Local ) {
+			return;
+		}
+
+		if ( ! wp_mkdir_p( $storage->get_base_directory() ) ) {
+			return;
+		}
+
+		$htaccess->add_rules( [ 'deny from all' ] );
+		$htaccess->save();
 	}
 }
