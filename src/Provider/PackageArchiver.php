50da451855cc2723f9b02f0de8266b12db39ef65
Brady Vercher <brady@blazersix.com>
2018-06-26 23:29:38 -0700

    Cache pending updates to make them available in the Composer repository.

--- src/Provider/PackageArchiver.php
+++ src/Provider/PackageArchiver.php
@@ -14,6 +14,7 @@ namespace SatisPress\Provider;
 use Cedaro\WP\Plugin\AbstractHookProvider;
 use SatisPress\Package;
 use SatisPress\PackageManager;
+use SatisPress\Release;
 
 /**
  * Package archiver class.
@@ -45,12 +46,58 @@ class PackageArchiver extends AbstractHookProvider {
 	public function register_hooks() {
 		// Cache the existing version of a plugin before it's updated.
 		if ( apply_filters( 'satispress_cache_packages_before_update', true ) ) {
-			add_filter( 'upgrader_pre_install', [ $this, 'cache_package_before_update' ], 10, 2 );
+			add_filter( 'upgrader_pre_install', [ $this, 'archive_source_before_update' ], 10, 2 );
 		}
+
+		add_filter( 'pre_set_site_transient_update_plugins', [ $this, 'archive_updates' ], 9999 );
+		add_filter( 'pre_set_site_transient_update_themes',  [ $this, 'archive_updates' ], 9999 );
 
 		// Delete the 'satispress_packages' transient.
 		add_action( 'upgrader_process_complete', [ $this, 'flush_packages_cache' ] );
 		add_action( 'set_site_transient_update_plugins', [ $this, 'flush_packages_cache' ] );
+	}
+
+	/**
+	 * Archive updates as they become available.
+	 *
+	 * @since 0.3.0
+	 *
+	 * @param  object $value Update transient value.
+	 * @return object
+	 */
+	public function archive_updates( $value ) {
+		if ( empty( $value->response ) ) {
+			return $value;
+		}
+
+		// The $id will be a theme slug or the plugin file.
+		foreach ( $value->response as $id => $update_data ) {
+			// Bail if a URL isn't available.
+			if ( empty( $update_data->package ) ) {
+				continue;
+			}
+
+			// Plugin data is stored as an object. Coerce to an array.
+			$update_data = (array) $update_data;
+
+			$type    = isset( $update_data['plugin'] ) ? 'plugin' : 'theme';
+			$package = $this->package_manager->get_package( $update_data[ $type ], $type );
+
+			// Bail if the package isn't whitelisted.
+			if ( empty( $package ) ) {
+				continue;
+			}
+
+			$release = new Release(
+				$package,
+				$update_data['new_version'],
+				$update_data['package']
+			);
+
+			$this->plugin->get_container()->get( 'release.manager' )->archive( $release );
+		}
+
+		return $value;
 	}
 
 	/**
@@ -62,7 +109,7 @@ class PackageArchiver extends AbstractHookProvider {
 	 * @param array $data   Extra data passed by the update/install process.
 	 * @return bool
 	 */
-	public function cache_package_before_update( bool $result, array $data ): bool {
+	public function archive_source_before_update( bool $result, array $data ): bool {
 		if ( empty( $data['plugin'] ) && empty( $data['theme'] ) ) {
 			return $result;
 		}
