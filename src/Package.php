8f2c10f14724643e85cdc526186c4b47c52a65ee
Brady Vercher <brady@blazersix.com>
2018-08-16 12:50:46 -0700

    Add missing methods to the Package interface after 7274e9d.

--- src/Package.php
+++ src/Package.php
@@ -47,6 +47,25 @@ interface Package {
 	public function get_description(): string;
 
 	/**
+	 * Retrieve the package directory.
+	 *
+	 * @since 0.3.0
+	 *
+	 * @return string
+	 */
+	public function get_directory(): string;
+
+	/**
+	 * Retrieve the list of files in the package.
+	 *
+	 * @since 0.3.0
+	 *
+	 * @param array $excludes Optional. Array of file names to exclude.
+	 * @return array
+	 */
+	public function get_files( array $excludes = [] ): array;
+
+	/**
 	 * Retrieve the homepage URL.
 	 *
 	 * @since 0.3.0
@@ -63,6 +82,16 @@ interface Package {
 	 * @return string
 	 */
 	public function get_name(): string;
+
+	/**
+	 * Retrieve the path to a file in the package.
+	 *
+	 * @since 0.3.0
+	 *
+	 * @param string $path Optional. Path relative to the package root.
+	 * @return string
+	 */
+	public function get_path( string $path = '' ): string;
 
 	/**
 	 * Retrieve the slug.
@@ -121,14 +150,31 @@ interface Package {
 	public function get_releases(): array;
 
 	/**
-	 * Retrieve the latest release.
+	 * Retrieve the installed version.
 	 *
 	 * @since 0.3.0
 	 *
-	 * @throws InvalidReleaseVersion If the package doesn't have any releases.
+	 * @return string
+	 */
+	public function get_installed_version(): string;
+	/**
+	 * Retrieve the installed release.
+	 *
+	 * @since 0.3.0
+	 *
 	 * @return Release
 	 */
-	public function get_latest_release(): Release;
+	public function get_installed_release(): Release;
+
+	/**
+	 * Whether a given release is the currently installed version.
+	 *
+	 * @since 0.3.0
+	 *
+	 * @param Release $release Release.
+	 * @return bool
+	 */
+	public function is_installed_release( Release $release ): bool;
 
 	/**
 	 * Retrieve the version for the latest release.
@@ -141,6 +187,16 @@ interface Package {
 	public function get_latest_version(): string;
 
 	/**
+	 * Retrieve the latest release.
+	 *
+	 * @since 0.3.0
+	 *
+	 * @throws InvalidReleaseVersion If the package doesn't have any releases.
+	 * @return Release
+	 */
+	public function get_latest_release(): Release;
+
+	/**
 	 * Retrieve a link to download the latest release.
 	 *
 	 * @since 0.3.0
@@ -151,12 +207,11 @@ interface Package {
 	public function get_latest_download_url(): string;
 
 	/**
-	 * Whether a given release is the currently installed version.
+	 * Whether an update is available.
 	 *
 	 * @since 0.3.0
 	 *
-	 * @param Release $release Release.
 	 * @return bool
 	 */
-	public function is_installed_release( Release $release ): bool;
+	public function is_update_available(): bool;
 }
