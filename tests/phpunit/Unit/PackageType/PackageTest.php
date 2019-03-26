de51580204239ebb471f444bf9fffaffb2b141a0
Brady Vercher <brady@blazersix.com>
2019-03-26 10:11:38 -0700

    Reduce dependencies in the Package and PackageBuilder unit tests.

--- tests/phpunit/Unit/PackageType/PackageTest.php
+++ tests/phpunit/Unit/PackageType/PackageTest.php
@@ -3,110 +3,103 @@ declare ( strict_types = 1 );
 
 namespace SatisPress\Test\Unit\PackageType;
 
-use Psr\Log\NullLogger;
-use SatisPress\Archiver;
 use SatisPress\Exception\PackageNotInstalled;
 use SatisPress\Package;
 use SatisPress\PackageType\BasePackage;
 use SatisPress\PackageType\PackageBuilder;
 use SatisPress\ReleaseManager;
-use SatisPress\Storage\Local as LocalStorage;
 use SatisPress\Test\Unit\TestCase;
 
 class PackageTest extends TestCase {
 	public function setUp(): void {
 		parent::setUp();
 
-		$archiver = new Archiver( new NullLogger() );
-		$storage  = new LocalStorage( SATISPRESS_TESTS_DIR . '/Fixture/wp-content/uploads/satispress/packages' );
-		$manager  = new ReleaseManager( $storage, $archiver );
-		$package  = new BasePackage();
-
-		$this->builder = new PackageBuilder( $package, $manager );
+		$this->package = new class extends BasePackage {
+			public function __set( $name, $value ) {
+				$this->$name = $value;
+			}
+		};
 	}
 
 	public function test_implements_package_interface() {
-		$package = $this->builder->build();
-
-		$this->assertInstanceOf( Package::class, $package );
+		$this->assertInstanceOf( Package::class, $this->package );
 	}
 
 	public function test_author() {
 		$expected = 'Cedaro';
-		$package  = $this->builder->set_author( $expected )->build();
+		$this->package->author = $expected;
 
-		$this->assertSame( $expected, $package->get_author() );
+		$this->assertSame( $expected, $this->package->get_author() );
 	}
 
 	public function test_author_url() {
 		$expected = 'https://www.cedaro.com/';
-		$package  = $this->builder->set_author_url( $expected )->build();
+		$this->package->author_url = $expected;
 
-		$this->assertSame( $expected, $package->get_author_url() );
+		$this->assertSame( $expected, $this->package->get_author_url() );
 	}
 
 	public function test_description() {
 		$expected = 'A package description.';
-		$package  = $this->builder->set_description( $expected )->build();
+		$this->package->description = $expected;
 
-		$this->assertSame( $expected, $package->get_description() );
+		$this->assertSame( $expected, $this->package->get_description() );
 	}
 
 	public function test_directory() {
-		$expected = __DIR__;
-		$package  = $this->builder->set_directory( $expected )->build();
+		$expected = __DIR__ . '/';
+		$this->package->directory = $expected;
 
-		$this->assertSame( $expected . '/', $package->get_directory() );
+		$this->assertSame( $expected, $this->package->get_directory() );
 	}
 
 	public function test_homepage() {
 		$expected = 'https://www.cedaro.com/';
-		$package  = $this->builder->set_homepage( $expected )->build();
+		$this->package->homepage = $expected;
 
-		$this->assertSame( $expected, $package->get_homepage() );
+		$this->assertSame( $expected, $this->package->get_homepage() );
 	}
 
 	public function test_is_installed() {
-		$package = $this->builder->build();
-		$this->assertFalse( $package->is_installed() );
+		$this->assertFalse( $this->package->is_installed() );
 
-		$package = $this->builder->set_installed( true )->build();
-		$this->assertTrue( $package->is_installed() );
+		$this->package->is_installed = true;
+		$this->assertTrue( $this->package->is_installed() );
 	}
 
 	public function test_installed_version() {
 		$expected = '1.0.0';
-		$package  = $this->builder->set_installed( true )->set_installed_version( $expected )->build();
+		$this->package->is_installed = true;
+		$this->package->installed_version = $expected;
 
-		$this->assertSame( $expected, $package->get_installed_version() );
+		$this->assertSame( $expected, $this->package->get_installed_version() );
 	}
 
 	public function test_get_installed_version_throws_exception_when_plugin_not_installed() {
 		$this->expectException( PackageNotInstalled::class );
 
-		$expected = '1.0.0';
-		$package  = $this->builder->set_installed_version( $expected )->build();
-		$package->get_installed_version();
+		$this->package->installed_version = '1.0.0';
+		$this->package->get_installed_version();
 	}
 
 	public function test_name() {
 		$expected = 'SatisPress';
-		$package  = $this->builder->set_name( $expected )->build();
+		$this->package->name = $expected;
 
-		$this->assertSame( $expected, $package->get_name() );
+		$this->assertSame( $expected, $this->package->get_name() );
 	}
 
 	public function test_slug() {
 		$expected = 'satispress';
-		$package  = $this->builder->set_slug( $expected )->build();
+		$this->package->slug = $expected;
 
-		$this->assertSame( $expected, $package->get_slug() );
+		$this->assertSame( $expected, $this->package->get_slug() );
 	}
 
 	public function test_type() {
 		$expected = 'plugin';
-		$package  = $this->builder->set_type( $expected )->build();
+		$this->package->type = $expected;
 
-		$this->assertSame( $expected, $package->get_type() );
+		$this->assertSame( $expected, $this->package->get_type() );
 	}
 }
