d05da24d35306c840f7cb548aec0123dd67c9292
saasfreelancer <syed.sayal.shah@gmail.com>
2020-06-21 14:46:35 +0500

    ignore windows file path warning, which fails the download.
    on windows / wamp server, this throws the issue if file is downloaded directly from dashboard, so this workaround allows for proper download

--- src/HTTP/ResponseBody/FileBody.php
+++ src/HTTP/ResponseBody/FileBody.php
@@ -35,7 +35,18 @@ class FileBody implements ResponseBody {
 	 * @throws InvalidFileName If the file name fails validation.
 	 */
 	public function __construct( string $filename ) {
+
 		$result = validate_file( $filename );
+
+		// if windows file path is found, ignore in
+		// in case wamp or such server is used
+		if ( 2 === $result ) {
+			$result = 0;
+
+			// normalize windows file path
+			$filename = wp_normalize_path( $filename );
+		}
+
 		if ( 0 !== $result ) {
 			throw InvalidFileName::withValidationCode( $filename, $result );
 		}
