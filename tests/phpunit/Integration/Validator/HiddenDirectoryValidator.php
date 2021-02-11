de04646c8605ca578a0ff8804173e3fc54901149
Brady Vercher <brady@blazersix.com>
2021-02-10 17:10:31 -0800

    Move validator tests to the integration suite and remove the PclZip dependency. Fixes #149
    WordPress uses a patched version of PclZip and in certain situations the
    development dependency that was used for tests could cause issues.

--- tests/phpunit/Integration/Validator/HiddenDirectoryValidator.php
+++ tests/phpunit/Integration/Validator/HiddenDirectoryValidator.php
@@ -1,7 +1,7 @@
 <?php
 declare ( strict_types = 1 );
 
-namespace SatisPress\Test\Unit\Validator;
+namespace SatisPress\Test\Integration\Validator;
 
 use SatisPress\Exception\InvalidPackageArtifact;
 use SatisPress\Release;
