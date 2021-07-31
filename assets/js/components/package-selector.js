c36dd29a0e7e0c5992887d4b2596b561aa77dfee
david <david@appsaloon.be>
2021-07-31 19:45:52 +0200

    Added a search field in package-selector.js

--- assets/js/components/package-selector.js
+++ assets/js/components/package-selector.js
@@ -1,9 +1,9 @@
 import { components, data, element, html, i18n } from '../utils/index.js';
 import { closeSmallIcon } from './icons.js';
 
-const { Button, CheckboxControl, Panel, PanelHeader, TabPanel } = components;
+const { Button, CheckboxControl, Panel, PanelHeader, TabPanel, TextControl } = components;
 const { useSelect } = data;
-const { Component } = element;
+const { useState } = element;
 const { __ } = i18n;
 
 function PackageSelector( props ) {
@@ -13,6 +13,8 @@ function PackageSelector( props ) {
 		onClose,
 		packages,
 	} = props;
+
+	const [ searchTerm, setSearchTerm ] = useState( '' );
 
 	const installedPackages = {
 		plugins: useSelect( ( select ) => select( 'satispress/packages' ).getPlugins() ),
@@ -46,8 +48,17 @@ function PackageSelector( props ) {
 
 	const tabContent = ( tab ) => {
 		const items = installedPackages[ tab.name ];
+		const searchTerms = searchTerm.toLowerCase().split(' ').filter( searchTerm => searchTerm );
+		const filteredItems = searchTerms.length === 0 ? items : items.filter( item => {
+			return searchTerms.some( searchTerm => {
+				const slugMatch = item.slug.toLowerCase().includes( searchTerm );
+				const nameMatch = item.name.toLowerCase().includes( searchTerm );
+				const authorMatch = item.author.toLowerCase().includes( searchTerm );
+				return slugMatch || nameMatch || authorMatch;
+			} );
+		} );
 
-		const listItems = items.map( item => {
+		const listItems = filteredItems.map( item => {
 			const { name, slug, type } = item;
 
 			return html`
@@ -61,7 +72,16 @@ function PackageSelector( props ) {
 			`;
 		} );
 
-		return html`<ul>${ listItems }</ul>`;
+		return html`<div>
+				<${ TextControl }
+					label=${ __( 'Search', 'satispress' ) + ' ' + tab.title }
+					hideLabelFromVision
+					placeholder=${ __( 'Search', 'satispress' ) + ' ' + tab.title }
+					onChange=${ setSearchTerm }
+					value=${ searchTerm }
+				/>
+			<ul>${ listItems }</ul>
+		</div>`;
 	};
 
 	return html`
