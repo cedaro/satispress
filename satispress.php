aecaa9bb390d3270a5496ca4dcf1099e2a72bc76
Austin Passy <367897+thefrosty@users.noreply.github.com>
2023-11-18 13:58:05 -0800

    Fix release node build (#2)
    * Don't be picky on dist files.
    
    * Version 2.0.1.
    
    * Remove makepot script on release action as it's throwing a fatal error.

--- satispress.php
+++ satispress.php
@@ -10,7 +10,7 @@
  * Plugin Name: SatisPress
  * Plugin URI: https://github.com/cedaro/satispress
  * Description: Generate a Composer repository from installed WordPress plugins and themes.
- * Version: 2.0.0
+ * Version: 2.0.1
  * Author: Cedaro
  * Author URI: https://www.cedaro.com/
  * License: GPL-2.0-or-later
@@ -37,7 +37,7 @@ if ( ! \defined( 'ABSPATH' ) ) {
  *
  * @var string
  */
-const VERSION = '2.0.0';
+const VERSION = '2.0.1';
 
 // Load the Composer autoloader.
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
